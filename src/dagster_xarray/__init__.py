f8a0dde3b910ecc1eb1e6311e9060c706eb06c7e
Mike Blackett <27811528+mikeblackett@users.noreply.github.com>
2026-06-28 23:04:33 +0100

    add pandera schema

--- src/dagster_xarray/__init__.py
+++ src/dagster_xarray/__init__.py
@@ -1,6 +1,8 @@
 from dagster_xarray.io_manager import NetCDFXarrayIOManager, ZarrXarrayIOManager
+from dagster_xarray.schema import pandera_schema_to_dagster_type
 
 __all__ = [
     "NetCDFXarrayIOManager",
     "ZarrXarrayIOManager",
+    "pandera_schema_to_dagster_type",
 ]
