c95373fcd6950121baa4734a85bef55c11eda5ba
Mike Blackett <27811528+mikeblackett@users.noreply.github.com>
2026-06-30 21:29:01 +0100

    rename schema.py to dagster_type.py

--- src/dagster_xarray/__init__.py
+++ src/dagster_xarray/__init__.py
@@ -2,7 +2,7 @@ from dagster_xarray.io_manager import (
     NetCDFXarrayIOManager,
     ZarrXarrayIOManager,
 )
-from dagster_xarray.schema import pandera_schema_to_dagster_type
+from dagster_xarray.dagster_type import pandera_schema_to_dagster_type
 from dagster_xarray.asset_checks import build_xarray_frequency_check
 
 __all__ = [
